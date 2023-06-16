31c42441834e189e3e74a0024e85a9436c87ca77
omodev <omooladev@gmail.com>
2023-06-16 12:49:27 +0100

    working on transforming the image to base64

--- shop-project/public/scripts/form.js
+++ shop-project/public/scripts/form.js
@@ -1,5 +1,33 @@
 const productName = document.querySelector("#product_name");
 const productPrice = document.querySelector("#product_price");
 const productImage = document.querySelector("#product_image");
+const formReply = document.querySelector(".form_reply");
+const productForm = document.querySelector(".product_form");
 
-console.log(productName, productPrice, productImage);
+//important-------->functions
+
+const submitFormHandler = async (event) => {
+  event.preventDefault();
+  formReply.innerHTML = "";
+  formReply.classList.remove("error");
+  formReply.classList.remove("success");
+  if (!productName.value || !productPrice.value || !productImage.value) {
+    formReply.innerHTML = "An error has occurred, please try again";
+    formReply.classList.add("error");
+    return;
+  }
+  let product = {
+    productName: productName.value,
+    productPrice: productPrice.value,
+    productImage: productImage.value,
+  };
+  const productImgTransform = await transformProductImage(product.productImage);
+  console.log(productImgTransform);
+  formReply.innerHTML = "Product item added successfully";
+  formReply.classList.add("success");
+};
+const transformProductImage = async (productImage) => {
+  const fileReader = new FileReader();
+};
+//important-------->Event Listeners
+productForm.addEventListener("submit", submitFormHandler);
