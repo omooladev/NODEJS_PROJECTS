6e958d88d0dfccee053df717a4a33d78774ec135
omodev <omooladev@gmail.com>
2023-07-16 18:57:49 +0100

    added push notification

--- shop-project/public/js/cart.js
+++ shop-project/public/js/cart.js
@@ -1,19 +1,41 @@
 const numberOfCartItems = document.querySelector(".cart_number");
 const addToCartButton = document.querySelectorAll(".add_to_cart");
+const notification = document.querySelector(".notification");
+const closeNotification = document.querySelector(".close_notification");
 
+let timeOut;
 const addProductToCartHandler = async (event) => {
   //----------> here we save the id of the product in the product element
   const productId = event.target.value;
-
+  event.target.disabled = true;
   //send a request to add product to cart
+
   try {
     const {
       data: { cart },
     } = await axios.post(`/cart/add/${productId}`);
     numberOfCartItems.innerHTML = cart.numberOfCartItems;
+    //---------->show notification
+    notification.classList.remove("hide");
+    //----------> clear timeout if it exists
+    if (timeOut) {
+      clearTimeout(timeOut);
+    }
+    //----------> add a timeout
+    timeOut = setTimeout(() => {
+      closeNotificationHandler();
+    }, 2000);
   } catch (error) {
     console.log(error);
   }
+  event.target.disabled = false;
+};
+const closeNotificationHandler = () => {
+  if (timeOut) {
+    clearTimeout(timeOut);
+    console.log("cleared", timeOut);
+  }
+  notification.classList.add("hide");
 };
 
 //----------> loop through all the cart button
@@ -34,4 +56,5 @@ const getCartContents = async () => {
   }
 };
 
+closeNotification.onclick = (event) => closeNotificationHandler(event);
 getCartContents();
